4654e95fbdbdc742b5bc48a6179ef4563db81a15
Anthony M <expopulation@gmail.com>
2023-01-31 15:27:15 +0200

    the bot can now get up

--- main.py
+++ main.py
@@ -3,3 +3,26 @@
 # Author: @Anthony01M
 # License: GPL-3.0
 ###
+
+# imports
+import discord
+import openai
+import os
+import datetime
+import asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Bot(intents=intents, activity=discord.Activity(
+    type=2, name="https://github.com/Berry-Studios/ChatGPTBot"), status=discord.Status.do_not_disturb)
+
+@client.event
+async def on_ready():
+    print(f"Bot Logged in as: {client.user.name}#{client.user.discriminator}")
+
+load_dotenv()
+
+client.run(os.getenv("TOKEN"))
